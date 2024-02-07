e80338e018f3028d216c3d21cd9bb4df383976d9
Victoria Vu <victoriamvvu@gmail.com>
2024-02-06 20:23:11 -0800

    Update db, settings and urls

--- jobapp/urls.py
+++ jobapp/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
 
-def hello(request):
-    return HttpResponse("Hello World")
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", hello),
+    path("", include("app.urls")),
 ]
