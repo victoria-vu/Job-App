b9d6a42af41cf92e6dc0350e7b333c014ac86c3d
Victoria Vu <victoriamvvu@gmail.com>
2024-02-20 12:20:48 -0800

    Modify admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from app.models import JobPost
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "salary")
+    list_filter = ("date", "salary", "expiry")
+    search_fields = ("title", "description")
+    search_help_text = "Write in your query and hit enter"
+    fieldsets = (
+        ("Basic Information", {
+            "fields": ("title", "description")
+        }),
+        ("More Information", {
+            "classes": ("collapse", "wide"),
+            "fields": (("expiry", "salary"), "slug")
+        }),
+    )
+
 
 # Register your models here.
+admin.site.register(JobPost, JobAdmin)
