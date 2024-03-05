722ab6fa8a610fb1a297a62d3228c684e12cb7fd
Victoria Vu <victoriamvvu@gmail.com>
2024-03-05 13:43:50 -0800

    Register JobPost, Location, Author, Skills models to admin page

--- app/admin.py
+++ app/admin.py
@@ -1,21 +1,8 @@
 from django.contrib import admin
-from app.models import JobPost
-
-class JobAdmin(admin.ModelAdmin):
-    list_display = ("title", "date", "salary")
-    list_filter = ("date", "salary", "expiry")
-    search_fields = ("title", "description")
-    search_help_text = "Write in your query and hit enter"
-    fieldsets = (
-        ("Basic Information", {
-            "fields": ("title", "description")
-        }),
-        ("More Information", {
-            "classes": ("collapse", "wide"),
-            "fields": (("expiry", "salary"), "slug")
-        }),
-    )
-
+from app.models import JobPost, Location, Author, Skills
 
 # Register your models here.
-admin.site.register(JobPost, JobAdmin)
+admin.site.register(JobPost)
+admin.site.register(Location)
+admin.site.register(Author)
+admin.site.register(Skills)
