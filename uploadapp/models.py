fecbc917f2080a1431a2f1ba6e3b8dae168c0cc9
Victoria Vu <victoriamvvu@gmail.com>
2024-03-18 13:22:29 -0700

    Modify model UploadFile to JobApplication to include fields relevant to job app, including job, first and last name, email, applied date

--- uploadapp/models.py
+++ uploadapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from app.models import JobPost
+
 
 # Create your models here.
 class Uploads(models.Model):
@@ -11,11 +13,16 @@ class Uploads(models.Model):
         return self.description
     
 
-class UploadFile(models.Model):
-    """A file upload."""
+class JobApplication(models.Model):
+    """A job application upload."""
 
+    job = models.ForeignKey(JobPost, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    applied_date = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to="files")
     description = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.description
+        return f"Application for {self.job.title} by {self.name}"
